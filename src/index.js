43024c30a39343b02d903499078a74c48f2e611b
Parker Fisher <pfishe13@kent.edu>
2022-08-16 11:22:23 -0400

    Fixed Weather fetching. Barebones UI

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
-import Weather from './weather';
 import UI from './ui';
 import './styles.css';
 
-function newLocation(city) {
-  let yeah = Weather(city);
-  yeah.setData();
-  // console.log(yeah);
-  return yeah;
+let defaultLocation = 'Miami';
+
+async function getWeatherData(location) {
+  const requestURL = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=e39170d30aebc36d04505fbbfd50451c
+    `;
+  const request = new Request(requestURL);
+  const response = await fetch(requestURL, {
+    mode: 'cors',
+  });
+  const responseData = await response.json();
+  console.log(responseData);
+  let weatherObject = parseWeatherData(responseData);
+  UI.updateUI(weatherObject);
 }
 
-let yup = newLocation('Akron');
+function parseWeatherData(dataJSON) {
+  let weatherObject = {
+    cityName: dataJSON.name,
+    kelvin: dataJSON['main']['temp'],
+    feelsKelvin: dataJSON['main']['feels_like'],
+    fahrenheit: toFahr(dataJSON['main']['temp']),
+    feelsFahrenheit: toFahr(dataJSON['main']['feels_like']),
+    celsius: toCels(dataJSON['main']['temp']),
+    feelsCelsius: toFahr(dataJSON['main']['feels_like']),
+    description: dataJSON['weather'][0]['description'],
+    wind: dataJSON['wind']['speed'],
+    humidity: dataJSON['main']['humidity'],
+  };
+  console.log(weatherObject);
+  return weatherObject;
+}
 
-console.log('UI updating', yup);
-UI.updateUI(yup);
+function toFahr(kelv) {
+  return Math.round(1.8 * (kelv - 273) + 32);
+}
+
+function toCels(kelv) {
+  return Math.round(kelv - 273.15);
+}
+
+getWeatherData(defaultLocation);
--- src/ui.js
+++ src/ui.js
@@ -3,12 +3,28 @@ import Weather from './weather';
 const UI = (() => {
   let celsius = false;
 
+  const toggleCelsius = () => {
+    celsius = !celsius;
+  };
+
   const updateUI = (weatherObject) => {
-    console.log(`The name of the city is ${weatherObject.getCity()}`);
+    console.log(`The name of the city is ${weatherObject['cityName']}`);
     // updateBackgroundImage();
-    updateCityHeader(weatherObject.getCity());
-    // updateLeftSideData();
-    // updateRightSideData();
+    updateCityHeader(weatherObject['cityName']);
+    celsius
+      ? updateLeftSideData(weatherObject['celsius'])
+      : updateLeftSideData(weatherObject['fahrenheit']);
+
+    const feelsTemp = celsius
+      ? weatherObject['feelsCelsius']
+      : weatherObject['feelsFahrenheit'];
+
+    updateRightSideData(
+      weatherObject['description'],
+      feelsTemp,
+      weatherObject['humidity'],
+      weatherObject['wind']
+    );
   };
 
   const updateCityHeader = (city) => {
@@ -17,7 +33,50 @@ const UI = (() => {
     cityHeader.textContent = city;
   };
 
-  return { updateUI };
+  const updateLeftSideData = (temp) => {
+    const leftHeader = document.getElementById('temperature');
+    leftHeader.textContent = temp;
+  };
+
+  const updateRightSideData = (description, feelsTemp, humidity, wind) => {
+    const rightContainer = document.getElementById('details');
+
+    const descriptionContainer = document.createElement('div');
+    const descriptionHeader = document.createElement('h3');
+    descriptionHeader.textContent = description;
+    descriptionContainer.appendChild(descriptionHeader);
+
+    const feelsTempContainer = document.createElement('div');
+    const feelsTempHeader = document.createElement('h4');
+    feelsTempHeader.textContent = `Feels Like`;
+    const feelsTempData = document.createElement('h4');
+    feelsTempData.textContent = feelsTemp;
+    feelsTempContainer.appendChild(feelsTempHeader);
+    feelsTempContainer.appendChild(feelsTempData);
+
+    const humidityContainer = document.createElement('div');
+    const humidityHeader = document.createElement('h4');
+    humidityHeader.textContent = `Humidity`;
+    const humidityData = document.createElement('h4');
+    humidityData.textContent = `${humidity}%`;
+    humidityContainer.appendChild(humidityHeader);
+    humidityContainer.appendChild(humidityData);
+
+    const windContainer = document.createElement('div');
+    const windHeader = document.createElement('h4');
+    windHeader.textContent = `Wind`;
+    const windData = document.createElement('h4');
+    windData.textContent = `${wind} MPH`;
+    windContainer.appendChild(windHeader);
+    windContainer.appendChild(windData);
+
+    rightContainer.appendChild(descriptionContainer);
+    rightContainer.appendChild(feelsTempContainer);
+    rightContainer.appendChild(humidityContainer);
+    rightContainer.appendChild(windContainer);
+  };
+
+  return { updateUI, toggleCelsius };
 })();
 
 export default UI;
